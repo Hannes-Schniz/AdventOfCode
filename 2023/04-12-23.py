e9c088ef5d31a60df9c64994557906f3806a55a7
Hannes <hannes.schniz@gmx.de>
2023-12-04 10:53:08 +0100

    added input parser

--- 2023/04-12-23.py
+++ 2023/04-12-23.py
@@ -1,9 +1,14 @@
 import os
 
+# parse input into scratch and winning numbers
+# check for each scratch number if the winning numbers contains it
+# add up counter for ech containing number
+# calc 2^counter
+# add up all results
+
 #-------------------------------------------------------------------------
 #                               Part 1
 #-------------------------------------------------------------------------
-
 
 #-------------------------------------------------------------------------
 #                               Part 2
@@ -16,11 +21,40 @@ import os
 #-------------------------------------------------------------------------
 
 dirname = os.path.dirname(__file__)
-file_path = os.path.join(dirname, 'input_03-12-23.txt')
+file_path = os.path.join(dirname, 'input_04-12-23-test.txt')
+
+testresult = 13
+
+testRows= [8,2,2,1,0,0]
+
+scratchNumbers = []
+
+winningNumbers = []
+
+def parseInput(line):
+    parsed = []
+    values = line.split(":")
+    values = values[1].split("|")
+    for value in values:
+        value = value.strip()
+        numbers = []
+        for number in value.split(" "):
+            if number != "":
+                numbers.append(number)
+        parsed.append(numbers)
+    return parsed
+    
+
+
 
 
 with open(file_path, "r") as file:
     
     #TODO fill with code
-    
+    line = file.readline()
+    parsedInput = parseInput(line)
+    scratchNumbers = parsedInput[0]
+    winningNumbers = parsedInput[1]
+    print(scratchNumbers)
+    print(winningNumbers)
     
