65c9d3c863198992b964ed1616cc1167e16360b4
hannes.schniz <hannes.schniz@gmx.de>
2023-12-05 15:40:53 +0100

    added new files

---
+++ 2023/05-12-23.py
@@ -0,0 +1,58 @@
+import os
+
+# parse input into scratch and winning numbers
+# check for each scratch number if the winning numbers contains it
+# add up counter for ech containing number
+# calc 2^counter
+# add up all results
+
+#-------------------------------------------------------------------------
+#                               Part 1
+#-------------------------------------------------------------------------
+
+
+        
+
+
+#-------------------------------------------------------------------------
+#                               Part 2
+#-------------------------------------------------------------------------
+
+
+        
+        
+        
+
+#-------------------------------------------------------------------------
+#                                General
+#-------------------------------------------------------------------------
+
+dirname = os.path.dirname(__file__)
+file_path = os.path.join(dirname, 'input_04-12-23.txt')
+
+    
+
+def parseInput(line):
+    parsed = []
+    values = line.split(":")
+    values = values[1].split("|")
+    for value in values:
+        value = value.strip()
+        numbers = []
+        for number in value.split(" "):
+            if number != "":
+                numbers.append(number)
+        parsed.append(numbers)
+    return parsed
+    
+
+
+
+
+with open(file_path, "r") as file:
+    
+    
+    print("Part 1: " + str(runOneResult))
+    print("Part 2: " + str(calcResultTwo()))
+
+    
