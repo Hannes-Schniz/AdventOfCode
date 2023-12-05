d5bcf6b2e8728407e88c89d808760fb213a3d8e5
hannes.schniz <hannes.schniz@gmx.de>
2023-12-05 18:35:14 +0100

    code not efficient enough

--- 2023/05-12-23.py
+++ 2023/05-12-23.py
@@ -15,8 +15,6 @@ class Mapping:
 #-------------------------------------------------------------------------
 #                               Part 1
 #-------------------------------------------------------------------------
-
-
         
 
 
@@ -24,10 +22,57 @@ class Mapping:
 #                               Part 2
 #-------------------------------------------------------------------------
 
+class Seed:
+    def __init__(self, start, seedRange):
+        self.start = start
+        self.seedRange = seedRange
+
+def reorgSeeds():
+    newSeeds = []
+    for i in range(0, len(seeds)):
+        if i%2 == 1:
+            continue
+        newSeeds.append(Seed(seeds[i], seeds[i+1]))
+    return newSeeds
+        
+def getLowestSoilIndexV2():
+    seeds = reorgSeeds()
+    firstRun = True
+    minSoilIndex = 0
+    idx = 1
+    for seed in seeds:
+        for i in range(seed.start, seed.start + seed.seedRange - 1):
+            currSoilIndex = getSoilIndex(i, 0)
+            if firstRun:
+                minSoilIndex = currSoilIndex
+                firstRun = False
+                continue
+            if currSoilIndex < minSoilIndex:
+                minSoilIndex = currSoilIndex
+            print_progress(idx, i, seed.start + seed.seedRange - 1)
+        idx += 1
+    return minSoilIndex       
+        
+def print_progress(seed, current_index, total_range):
+    progress = round(total_range -current_index )
+    print(f'Seed: {seed} Progress: {progress}')
+
+#-------------------------------------------------------------------------
+#                                General
+#-------------------------------------------------------------------------
+
+dirname = os.path.dirname(__file__)
+file_path = os.path.join(dirname, 'input_05-12-23.txt')
+
+def extractSeeds(seedRow):
+    working = seedRow.split(" ")
+    for j in range(1, len(working)):
+        seeds.append(int(working[j]))
+
 def getLowestSoilIndex():
     firstRun = True
     minSoilIndex = 0
-    print(seeds)
+    #print(seeds)
     for seed in seeds:
         currSoilIndex = getSoilIndex(seed, 0)
         if firstRun:
@@ -37,17 +82,6 @@ def getLowestSoilIndex():
         if currSoilIndex < minSoilIndex:
             minSoilIndex = currSoilIndex
     return minSoilIndex
-        
-        
-        
-
-#-------------------------------------------------------------------------
-#                                General
-#-------------------------------------------------------------------------
-
-dirname = os.path.dirname(__file__)
-file_path = os.path.join(dirname, 'input_05-12-23.txt')
-
 
 
 def stripStrings(strings):
@@ -57,19 +91,15 @@ def cleanMappings(dirtyMappings):
     cleanMappings = []
     for i in range(0, len(dirtyMappings)):
         if i == 0:
-            working = dirtyMappings[i][0].split(" ")
-            for j in range(1, len(working)):
-                seeds.append(int(working[j]))
+            extractSeeds(dirtyMappings[i][0])
             continue
         
-        #print(dirtyMappings[i][0][0])
         workingMapping = []
         for value in dirtyMappings[i]:
             if value[0][0].isnumeric() == False:
                 continue
             value = value.split(" ")
             workingMapping.append( Mapping(int(value[0]), int(value[1]), int(value[2])))
-            #print(cleanMappings[-1].source, cleanMappings[-1].target, cleanMappings[-1].mapRange)
         cleanMappings.append(workingMapping)
     return cleanMappings
 
@@ -79,13 +109,12 @@ def checkIfInMapRange(mapping, value):
 
 def getSoilIndex(targetIdx, step):
     if step == len(mappings):
-        print(targetIdx)
+        #print(targetIdx)
         return targetIdx
     for mapping in mappings[step]:
         if checkIfInMapRange(mapping, targetIdx):
             nextTarget = mapping.target + targetIdx - mapping.source
-            print(targetIdx, mapping.source, mapping.mapRange, mapping.target, nextTarget)
-            #print(targetIdx, nextTarget)
+            #print("Step " + str(step) + ": " + str(targetIdx) + " --> " + str(nextTarget))
             return getSoilIndex(nextTarget, step + 1)
     return getSoilIndex(targetIdx, step + 1)    
 
@@ -109,4 +138,5 @@ with open(file_path, "r") as file:
         return cleanMappings(dirtyMappings)
     
     mappings = parseInput()
-    print (getLowestSoilIndex())
+    print ("Part 1: " + str(getLowestSoilIndex()))
+    print ("Part 2: " + str(getLowestSoilIndexV2()))
