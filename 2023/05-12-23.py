c6f853724ef740d21fb7fd6f69042fb2e87a012f
hannes.schniz <hannes.schniz@gmx.de>
2023-12-05 16:43:57 +0100

    parsed input

--- 2023/05-12-23.py
+++ 2023/05-12-23.py
@@ -1,6 +1,16 @@
 import os
 
+seeds = []
 
+mappings= []
+
+soil = []  
+
+class Mapping:
+    def __init__(self, target, source, mapRange):
+        self.target = target
+        self.source = source
+        self.mapRange = mapRange
 
 #-------------------------------------------------------------------------
 #                               Part 1
@@ -24,31 +34,55 @@ import os
 #-------------------------------------------------------------------------
 
 dirname = os.path.dirname(__file__)
-file_path = os.path.join(dirname, 'input_04-12-23.txt')
-
-    
-
-def parseInput(line):
-    parsed = []
-    values = line.split(":")
-    values = values[1].split("|")
-    for value in values:
-        value = value.strip()
-        numbers = []
-        for number in value.split(" "):
-            if number != "":
-                numbers.append(number)
-        parsed.append(numbers)
-    return parsed
-    
+file_path = os.path.join(dirname, 'input_05-12-23-test.txt')
 
 
+
+def stripStrings(strings):
+    return [string.rstrip() for string in strings]
+
+def cleanMappings(dirtyMappings):
+    cleanMappings = []
+    for i in range(0, len(dirtyMappings)):
+        if i == 0:
+            working = dirtyMappings[i][0].split(" ")
+            for j in range(1, len(working)):
+                seeds.append(int(working[j]))
+            continue
+        
+        #print(dirtyMappings[i][0][0])
+        for value in dirtyMappings[i]:
+            if value[0][0].isnumeric() == False:
+                continue
+            value = value.split(" ")
+            cleanMappings.append( Mapping(int(value[0]), int(value[1]), int(value[2])))
+            #print(cleanMappings[-1].source, cleanMappings[-1].target, cleanMappings[-1].mapRange)
+    return cleanMappings
 
 
 with open(file_path, "r") as file:
     
+    def parseInput():
+        line = file.readline()
+        inputs = []
+        dirtyMappings = []
+        while line:
+            inputs.append(line)
+            line = file.readline()
+        builder = []
+        for value in inputs:
+            if value == "\n":
+                dirtyMappings.append(stripStrings(builder))
+                builder = []
+            else:
+                builder.append(value)
+        return cleanMappings(dirtyMappings)
     
-    print("Part 1: " + str(runOneResult))
-    print("Part 2: " + str(calcResultTwo()))
+    mappings = parseInput()
+    print(seeds)
+
+    
+
+
 
     
