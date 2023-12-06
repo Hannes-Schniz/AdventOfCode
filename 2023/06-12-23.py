01a0bd505c31ba828e21d072d0b19db40e5757ec
Hannes <hannes.schniz@gmx.de>
2023-12-06 12:58:40 +0100

    all done lol

--- 2023/06-12-23.py
+++ 2023/06-12-23.py
@@ -1,10 +1,51 @@
 import os
 
+import time
+import curses
+
+stdscr = curses.initscr()
+    
+
+class Race:
+    def __init__(self, time, distance):
+        self.time = time
+        self.distance = distance
+
+
 
 #-------------------------------------------------------------------------
 #                               Part 1
 #-------------------------------------------------------------------------
-        
+def printAndOverwrite(string):
+    stdscr.addstr(0, 0, string)
+    stdscr.refresh()
+             
+
+def calcDistance(time, speed):
+    return time * speed  
+    
+
+
+# 1 sec => (7s-1s) * 1 = 6
+# 2 sec => (7s-2s) * 2 = 10
+# (totalTime - timeCharged) * timeCharged = distance
+# 
+def findWinningTimes(race):
+    winnings = 0
+    for i in range(1, race.time ):
+        distance = calcDistance(race.time - i, i)
+        if distance > race.distance:
+            winnings +=1
+        printAndOverwrite(str(100/race.time * (race.time - i)) + "%")
+    print(winnings)
+    return winnings
+    
+
+def calcResult(races):
+    sumWinnings = 1
+    for race in races:
+        sumWinnings = findWinningTimes(race) * sumWinnings
+    return sumWinnings  
 
 
 #-------------------------------------------------------------------------
@@ -12,8 +53,16 @@ import os
 #-------------------------------------------------------------------------
 
 
-        
-        
+def mergeNumbers(races):
+
+    mergedTime = ""
+    mergedDistance = ""
+    for race in races:
+        mergedTime += str(race.time)
+        mergedDistance += str(race.distance)
+    print(str(mergedTime), str(mergedDistance))
+    return Race(int(mergedTime), int(mergedDistance))
+    
         
 
 #-------------------------------------------------------------------------
@@ -21,17 +70,29 @@ import os
 #-------------------------------------------------------------------------
 
 dirname = os.path.dirname(__file__)
-file_path = os.path.join(dirname, 'input_04-12-23.txt')
+file_path = os.path.join(dirname, 'input_06-12-23.txt')
 
 
+
+def parseInput(lineOne, lineTwo):
+    parsedInput = []
+    valuesTime = list(filter(None, lineOne.split(":")[1].strip().split(" ")))
+    valuesDist = list(filter(None, lineTwo.split(":")[1].strip().split(" ")) )
+    print(valuesTime)
+    print(valuesDist)   
+    for i in range(len(valuesTime)):
+        parsedInput.append(Race(int(valuesTime[i]), int(valuesDist[i])))
+    return parsedInput
+    
     
 
 
 
 
 with open(file_path, "r") as file:
+    parsedInput = parseInput(file.readline(), file.readline())
     
     
-    print("Part 1: " )
-    print("Part 2: " )
+    print("Part 1: " + str(calcResult(parsedInput)))
+    print("Part 2: " + str(findWinningTimes(mergeNumbers(parsedInput))))
     
