6a19332077d114eb0e8f4285d3c012b6993860b7
Hannes <hannes.schniz@gmx.de>
2023-12-07 11:22:24 +0100

    generated files

---
+++ 2023/07-12-23.py
@@ -0,0 +1,47 @@
+import os
+    
+
+
+
+
+#-------------------------------------------------------------------------
+#                               Part 1
+#-------------------------------------------------------------------------
+             
+ 
+
+
+#-------------------------------------------------------------------------
+#                               Part 2
+#-------------------------------------------------------------------------
+
+
+  
+
+#-------------------------------------------------------------------------
+#                                General
+#-------------------------------------------------------------------------
+
+dirname = os.path.dirname(__file__)
+file_path = os.path.join(dirname, 'input_07-12-23.txt')
+
+
+
+def parseInput(lineOne, lineTwo):
+    parsedInput = []
+    
+    return parsedInput
+    
+    
+
+
+
+
+with open(file_path, "r") as file:
+    
+    #TODO: Parse input
+    
+    print("TODO")
+    #print("Part 1: " + str(calcResult(parsedInput))  + "           ")
+    #print("Part 2: " + str(findWinningTimes(mergeNumbers(parsedInput))))
+    
