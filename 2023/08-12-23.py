45df5e2d12659f438a4cf66db6c844e44273468d
Hannes <hannes.schniz@gmx.de>
2023-12-08 21:52:19 +0100

    wrong output

--- 2023/08-12-23.py
+++ 2023/08-12-23.py
@@ -1,13 +1,36 @@
 import os, functools
 
+start = 0
 
+directions = []
 
+tree = {}
+
+def parseInput(line):
+    if(line == "\n"):
+        return
+    splittedLine = line.split(" = ")
+    splittedLine[1] = splittedLine[1].strip()
+    splittedLine[1] = splittedLine[1][1:-1].split(", ")
+    tree[splittedLine[0]] = splittedLine[1]
+    
 
 #-------------------------------------------------------------------------
 #                               Part 1
 #-------------------------------------------------------------------------
 
-
+def traverse():
+    currPos = start
+    count = 0
+    while currPos != "ZZZ":
+        for direction in directions:
+            if direction == 0:
+                currPos = tree[currPos][0]
+            elif direction == 1:
+                currPos = tree[currPos][1]
+            count += 1
+    return count
+        
 #-------------------------------------------------------------------------
 #                               Part 2
 #-------------------------------------------------------------------------
@@ -19,16 +42,29 @@ import os, functools
 #-------------------------------------------------------------------------
 
 dirname = os.path.dirname(__file__)
-file_path = os.path.join(dirname, 'input_07-12-23.txt')
+file_path = os.path.join(dirname, 'input_08-12-23.txt')
 
 with open(file_path, "r") as file:
     
     input = file.readline()
+    for char in input[0:-1]:
+        translated = 0
+        if char == "L":
+            translated = 0
+        else:
+            translated = 1
+        directions.append(translated)
+    input = file.readline()
+    input = file.readline()
+    start = input.split(" ")[0]
     while input:
+        parseInput(input)
         input = file.readline()
 
-    
-#print("Part 1: " + str(calcResult(sortStrength(handMap))))
+traverse()
+
+#x > 10530 11567
+print("Part 1: " + str(traverse()))
 
 #print("Part 2: " + str(calcResult(sortStrength(handHash))))
     
