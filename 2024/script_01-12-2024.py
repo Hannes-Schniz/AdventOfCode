8419ef41b2e70d11afc6367d44a87bbf75414c93
hannes <contact@hannesschniz.de>
2024-12-01 10:37:20 +0100

    cleaned up a bit

--- 2024/script_01-12-2024.py
+++ 2024/script_01-12-2024.py
@@ -7,8 +7,7 @@ class main:
             solution+=abs(left[i] - right[i])
         return solution
     def solutionTwo(lines):
-        left = sorted([int(x.split("   ")[0]) for x in lines])
-        right = sorted([int(x.split("   ")[1]) for x in lines])
-        return sum([right.count(x)*x for x in left])     
+        right = [int(x.split("   ")[0]) for x in lines]
+        return sum([right.count(x)*x for x in [int(x.split("   ")[0]) for x in lines]])     
         
         
