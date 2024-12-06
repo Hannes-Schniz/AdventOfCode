91ec5ba822fcbb1b4f421e63161a384dc9b08016
hannes <hannes.schniz@gmx.de>
2024-12-06 08:31:31 +0100

    cleanup solution 1

--- 2024/2024-12-06_script.py
+++ 2024/2024-12-06_script.py
@@ -1,24 +1,25 @@
 class main:
     def solutionOne(lines):
-        new_lines= lines
-        guard = main.find_guard(lines)
-        new_lines[guard[1]] = new_lines[guard[1]][:guard[0]] + 'S' + new_lines[guard[1]][guard[0]+1:]
-        steps = main.parole(lines, guard[1], guard[0], 'up', 0)
-        while steps[3] != 'done':
-            new_lines[steps[1]] = new_lines[steps[1]][:steps[2]] + 'X' + new_lines[steps[1]][steps[2]+1:]
-            steps = main.parole(steps[0], steps[1], steps[2], steps[3], steps[4])
-        return main.get_count(new_lines)
+        return main.get_count(main.traverse(lines), ['X'])
     def solutionTwo(lines):
         return ""
+    
+    def traverse(lines):
+        guard = main.find_guard(lines)
+        steps = main.parole(lines, guard[1], guard[0], 'up', 0)
+        while steps[3] != 'done':
+            steps = main.parole(steps[0], steps[1], steps[2], steps[3])
+        return steps[0]
     
     def print_newLines(lines):
         for line in lines:
             print(line)
     
-    def get_count(lines):
+    def get_count(lines, chars):
         solution = 0
         for line in lines:
-            solution += line.count('X') + line.count('S')
+            for char in chars:
+                solution += line.count(char)
         return solution 
     
     def find_guard(lines):
@@ -27,30 +28,35 @@ class main:
                 if lines[y][x] == '^':
                     return (x,y)
         return (-1,-1)
-    
-    def parole(lines, y, x, direction, steps):
+            
+    def parole(lines, y, x, direction):
         if x-1 < 0 or x+1 >= len(lines[0]) or y-1 < 0 or y+1 >= len(lines):
-            return [lines,y,x,'done',steps]
+            lines[y] = lines[y][:x] + 'X' + lines[y][x+1:]
+            return [lines,y,x,'done']
         match direction:
             case 'up':
                 if lines[y-1][x] == '#':
                     direction = 'right'
-                    return [lines,y,x,direction,steps]
-                return [lines,y - 1,x,direction,steps+1]
+                    return [lines,y,x,direction]
+                lines[y] = lines[y][:x] + 'X' + lines[y][x+1:]
+                return [lines,y - 1,x,direction]
             case 'right':
                 if lines[y][x+1] == '#':
                     direction = 'down'
-                    return [lines,y,x,direction,steps]
-                return [lines,y,x + 1,direction,steps+1]
+                    return [lines,y,x,direction]
+                lines[y] = lines[y][:x] + 'X' + lines[y][x+1:]
+                return [lines,y,x + 1,direction]
             case 'down':
                 if lines[y+1][x] == '#':
                     direction = 'left'
-                    return [lines,y,x,direction,steps]
-                return [lines,y + 1,x,direction,steps+1]
+                    return [lines,y,x,direction]
+                lines[y] = lines[y][:x] + 'X' + lines[y][x+1:]
+                return [lines,y + 1,x,direction]
             case 'left':
                 if lines[y][x-1] == '#':
                     direction = 'up'
-                    return [lines,y,x,direction,steps]
-                return [lines,y,x - 1,direction,steps+1]
+                    return [lines,y,x,direction]
+                lines[y] = lines[y][:x] + 'X' + lines[y][x+1:]
+                return [lines,y,x - 1,direction]
                 
         
