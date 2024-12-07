936b7a0bb17efb388fcc79b37b12c8a7bc5ae7bb
hannes <hannes.schniz@gmx.de>
2024-12-07 13:45:58 +0100

    working on parser

---
+++ 2024/2024-12-07_script.py
@@ -0,0 +1,12 @@
+class main:
+    def solutionOne(lines):
+        return ''
+    def solutionTwo(lines):
+        return '' 
+    
+    def parse(lines):
+        parsed = []
+        for line in lines:
+            temp = lines.split(': ')
+            curr = [int(temp[0])]
+            
