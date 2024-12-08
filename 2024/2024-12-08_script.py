5729466c0ec5d384b924801af0c303a69dc0d009
hannes <hannes.schniz@gmx.de>
2024-12-08 19:30:16 +0100

    solved part 2 day 8 2024

--- 2024/2024-12-08_script.py
+++ 2024/2024-12-08_script.py
@@ -16,18 +16,52 @@ class main:
                     if main.isOnBoard(lines,diffTwo):
                         uniques.append((diffTwo))
         #main.printBoard(lines, uniques)
+        return len(set(uniques))      
+
+    def solutionTwo(lines):
+        lines = [[y for y in x] for x in main.clean_lines(lines)]
+        maps = main.split_maps(lines)
+        uniques = []
+        for key in maps.keys():
+            for posOne in maps[key]:
+                if len(maps[key]) > 1:
+                    uniques.append(posOne)
+                for posTwo in maps[key]:
+                    if posOne == posTwo:
+                        continue
+                    diff = main.subPositions(posOne,posTwo)
+                    #   print(posOne, posTwo)
+                    uniques += main.getAllPositions(lines, posOne, posTwo, diff)   
+        #main.printBoard(lines, uniques)       
         return len(set(uniques))
-                    
+    
+    def getAllPositions(lines, posOne, posTwo, diff):
+        line = main.findStart(lines, posTwo, diff) +    main.findEnd(lines, posOne, diff)
+        return line
+        
+        
+    def findStart(lines, posTwo, diff):
+        line = []
+        pos = main.subPositions(posTwo, diff)
+        while main.isOnBoard(lines, pos):
+            line.append(pos)
+            pos = main.subPositions(pos, diff)
+        return line
+    
+    def findEnd(lines, posOne, diff):
+        line = []
+        pos = main.addPositions(posOne, diff)
+        while main.isOnBoard(lines, pos):
+            line.append(pos)
+            pos = main.addPositions(pos, diff)
+        return line
+    
     def printBoard(lines, positions):
         for y in range(len(lines)):
             for x in range(len(lines[y])):
                 if (x,y) in positions:
                     lines[y][x] = '#'
             print(''.join(lines[y]))
-                                      
-
-    def solutionTwo(lines):
-        return ''
     
     def addPositions(posOne, posTwo):
         return (posOne[0]+posTwo[0],posOne[1]+posTwo[1])
