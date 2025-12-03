0761128822f9e0fc33b8bd148eb14fc4717a9e23
hannes <hannes.schniz@gmx.de>
2025-12-04 00:48:31 +0100

    added back go files

---
+++ go/2025/day2/02_script .go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("###################### Test ######################")
+	fmt.Println(part1("02_test.txt"))
+	fmt.Println(part2("02_test.txt"))
+	fmt.Println("###################### Input ######################")
+	fmt.Println(part1("02_input.txt"))
+	fmt.Println(part2("02_input.txt"))
+}
+
+func part1(filename string) int {
+	fmt.Println("<----------> Part1 <---------->")
+	return -1
+}
+func part2(filename string) int {
+	fmt.Println("<----------> Part2 <---------->")
+	return -1
+}
